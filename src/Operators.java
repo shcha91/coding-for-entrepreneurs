8d5cad286c1d1558af3ed640b78ed64ddc8651b4
RedRidingHoodChaCha <shcha91@gmail.com>
2017-10-15 13:11:58 +0900

    first commit

---
+++ src/Operators.java
@@ -0,0 +1,10 @@
+public class Operators {
+    public void calculate() {
+        System.out.println(1 + 2);
+        System.out.println(3 - 1);
+        System.out.println(5 * 4);
+
+        // special case
+        System.out.println(5 / 2);
+    }
+}
